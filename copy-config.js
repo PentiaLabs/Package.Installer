0c2cf237d81e68f1fdd72f7131cfa96a19779de8
Emil Okkels Klein <eok@pentia.dk>
2016-09-12 15:36:09 +0200

    fixed cwd path

--- copy-config.js
+++ copy-config.js
@@ -5,8 +5,8 @@ console.log('copying config files')
 var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 
 console.log('node path is ' + node_module_RootPath)
-console.log('cwd is ' + process.cwd())
+console.log('cwd is ' + process.env.INIT_CWD)
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
-gulp.src([node_solution_config, node_solution_packages]).pipe(gulp.dest(process.cwd(), { overwrite: false }))
+gulp.src([node_solution_config, node_solution_packages]).pipe(gulp.dest(process.env.INIT_CWD, { overwrite: false }))
