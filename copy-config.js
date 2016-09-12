b0839facb670f9e35173b20951d1a9ccb9e6049a
Emil Klein <eok@pentia.dk>
2016-09-12 21:15:49 +0200

    removed all unused npm dependencies, added new dependency to cwd for cwd resolving

--- copy-config.js
+++ copy-config.js
@@ -1,12 +1,14 @@
 var gulp = require("gulp")
 var path = require("path");
 var fs = require("fs");
+var cwd = require("cwd");
+
 console.log('copying config files')
 var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 
 console.log('node path is ' + node_module_RootPath)
-console.log('cwd is ' + process.env.INIT_CWD)
+console.log('cwd is ' + cwd())
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
-gulp.src([node_solution_config, node_solution_packages]).pipe(gulp.dest(process.env.INIT_CWD, { overwrite: false }))
+gulp.src([node_solution_config, node_solution_packages]).pipe(gulp.dest(cwd(), { overwrite: false }))
