fdc53e162d7a2d7065940649addabba6bc145beb
Emil Okkels Klein <eok@pentia.dk>
2016-09-12 15:27:49 +0200

    added log messages for debug purposes

--- copy-config.js
+++ copy-config.js
@@ -1,9 +1,10 @@
 var gulp = require("gulp")
 var path = require("path");
 var fs = require("fs");
-
+console.log('copying config files')
 var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 
+console.log('node path is ' + node_module_RootPath)
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
