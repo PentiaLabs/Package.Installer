de079d0941eb1d96fc0ab53262b5dc373d739ef4
Emil Okkels Klein <eok@pentia.dk>
2016-09-12 15:22:07 +0200

    fixed pipe in copy-config.js

--- copy-config.js
+++ copy-config.js
@@ -7,4 +7,4 @@ var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
-gulp.src([node_solution_config, node_solution_packages]).dest(process.cwd(), { overwrite: false })
+gulp.src([node_solution_config, node_solution_packages]).pipe(gulp.dest(process.cwd(), { overwrite: false }))
