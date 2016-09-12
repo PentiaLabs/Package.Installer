a4258044ed749e621e806f02215d6e5f997c900a
Emil Okkels Klein <eok@pentia.dk>
2016-09-12 15:30:39 +0200

    added log message for cwd

--- copy-config.js
+++ copy-config.js
@@ -5,6 +5,7 @@ console.log('copying config files')
 var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 
 console.log('node path is ' + node_module_RootPath)
+console.log('cwd is ' + process.cwd())
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
