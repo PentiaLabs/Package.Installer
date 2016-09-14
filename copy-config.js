4e30cd4735495d60e2ea98f90a0c38752b9f715d
Emil Okkels Klein <eok@pentia.dk>
2016-09-14 14:14:56 +0200

    added checks to not overwrite existing configs, this closes #2

--- copy-config.js
+++ copy-config.js
@@ -2,9 +2,21 @@ var gulp = require("gulp")
 var path = require("path");
 var fs = require("fs");
 var cwd = require("cwd");
+var fileExists = require("file-exists");
 
 var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
-gulp.src([node_solution_config, node_solution_packages]).pipe(gulp.dest("../../", { overwrite: false }))
+var dest_solution_packages = path.join(node_module_RootPath, "../../solution-packages.json")
+var dest_solution_config = path.join(node_module_RootPath, "../../solution-config.json")
+
+if(!fileExists(dest_solution_config))
+{
+    gulp.src(node_solution_config).pipe(gulp.dest("../.."))
+}
+
+if(!fileExists(dest_solution_packages))
+{
+    gulp.src(node_solution_packages).pipe(gulp.dest("../.."))
+}
