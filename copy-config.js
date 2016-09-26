454470c5b80a869a0bda3b61af963974ef363763
Emil Okkels Klein <eok@pentia.dk>
2016-09-26 08:40:04 +0200

    changed to native node.js check for file exist in copy script, bumped version 0.1.4

--- copy-config.js
+++ copy-config.js
@@ -2,21 +2,22 @@ var gulp = require("gulp")
 var path = require("path");
 var fs = require("fs");
 var cwd = require("cwd");
-var fileExists = require("file-exists");
 
 var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
 var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
 var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
 
-var dest_solution_packages = path.join(node_module_RootPath, "../../solution-packages.json")
-var dest_solution_config = path.join(node_module_RootPath, "../../solution-config.json")
+var dest_solution_packages = path.join(node_module_RootPath, "../../../solution-packages.json")
+var dest_solution_config = path.join(node_module_RootPath, "../../../solution-config.json")
 
-if(!fileExists(dest_solution_config))
-{
-    gulp.src(node_solution_config).pipe(gulp.dest("../.."))
+try {fs.accessSync(dest_solution_config, fs.F_OK);
+    console.log("Solution-config.json found, not copying")
+} catch (e) {
+    gulp.src(node_solution_config).pipe(gulp.dest("../../.."))
 }
 
-if(!fileExists(dest_solution_packages))
-{
-    gulp.src(node_solution_packages).pipe(gulp.dest("../.."))
+try {fs.accessSync(dest_solution_packages, fs.F_OK);
+    console.log("solution-packages.json found, not copying")
+} catch (e) {
+     gulp.src(node_solution_packages).pipe(gulp.dest("../../.."))
 }
