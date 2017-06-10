951ec90cd49d3eb5b677697d485aacc7ec75a06c
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-06-10 14:25:40 +0200

    Fxi ESlint errors
    Removed unsued dependencies, and used ESint to clean up the code.
    
    no-console is only a warn, we should hide this information behind a flag, or at least some pretty console log.

--- copy-config.js
+++ copy-config.js
@@ -1,23 +1,22 @@
-var gulp = require("gulp")
-var path = require("path");
-var fs = require("fs");
-var cwd = require("cwd");
+var gulp = require('gulp');
+var path = require('path');
+var fs = require('fs');
 
-var node_module_RootPath = path.dirname(fs.realpathSync(__filename))
-var node_solution_config = path.join(node_module_RootPath, "/solution-config.json");
-var node_solution_packages = path.join(node_module_RootPath, "/solution-packages.json");
+var node_module_RootPath = path.dirname(fs.realpathSync(__filename));
+var node_solution_config = path.join(node_module_RootPath, '/solution-config.json');
+var node_solution_packages = path.join(node_module_RootPath, '/solution-packages.json');
 
-var dest_solution_packages = path.join(node_module_RootPath, "../../../solution-packages.json")
-var dest_solution_config = path.join(node_module_RootPath, "../../../solution-config.json")
+var dest_solution_packages = path.join(node_module_RootPath, '../../../solution-packages.json');
+var dest_solution_config = path.join(node_module_RootPath, '../../../solution-config.json');
 
 try {fs.accessSync(dest_solution_config, fs.F_OK);
-    console.log("Solution-config.json found, not copying")
+	console.log('Solution-config.json found, not copying');
 } catch (e) {
-    gulp.src(node_solution_config).pipe(gulp.dest("../../.."))
+	gulp.src(node_solution_config).pipe(gulp.dest('../../..'));
 }
 
 try {fs.accessSync(dest_solution_packages, fs.F_OK);
-    console.log("solution-packages.json found, not copying")
+	console.log('solution-packages.json found, not copying');
 } catch (e) {
-     gulp.src(node_solution_packages).pipe(gulp.dest("../../.."))
+	gulp.src(node_solution_packages).pipe(gulp.dest('../../..'));
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,38 @@
 /*jslint node: true */
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var nopt = require("nopt");
-var path = require("path");
-var requireDir = require("require-dir");
-var build = require("./build.js");
-var powershell = require("./modules/powershell");
-var fs = require("fs");
+var gulp = require('gulp');
+var nopt = require('nopt');
+var path = require('path');
+var build = require('./build.js');
+var powershell = require('./modules/powershell');
+var fs = require('fs');
 
 var args = nopt({
-  "env"     : [String, null]
+	'env'     : [String, null]
 });
 
 build.setEnvironment(args.env);
 
-gulp.task("install-packages", function (callback) {
-  build.logEvent("builder", "Installing packages");
-  var psFile = path.join(path.dirname(fs.realpathSync(__filename)), "/powershell-scripts/Install-packages.ps1");
-  var packagesConfig = path.join(process.cwd(), "/solution-packages.json");
+gulp.task('install-packages', function (callback) {
+	build.logEvent('builder', 'Installing packages');
+	var psFile = path.join(path.dirname(fs.realpathSync(__filename)), '/powershell-scripts/Install-packages.ps1');
+	var packagesConfig = path.join(process.cwd(), '/solution-packages.json');
 
-  var websiteRoot = build.config.websiteRoot;
-  var websiteDataRoot = build.config.websiteDataRoot;
+	var websiteRoot = build.config.websiteRoot;
+	var websiteDataRoot = build.config.websiteDataRoot;
 
-  if(!path.isAbsolute(websiteRoot))
-  {
-    websiteRoot = path.join(process.cwd(),websiteRoot);
-  }
+	if(!path.isAbsolute(websiteRoot)) {
+		websiteRoot = path.join(process.cwd(),websiteRoot);
+	}
 
-  if(!path.isAbsolute(websiteDataRoot))
-  {
-    websiteDataRoot = path.join(process.cwd(),websiteDataRoot);
-  }
+	if(!path.isAbsolute(websiteDataRoot)) {
+		websiteDataRoot = path.join(process.cwd(),websiteDataRoot);
+	}
 
-  powershell.runAsync(psFile, " -packagesFileLocation '" + packagesConfig + "'" + " -webRootPath " + websiteRoot + " -dataRootPath " + websiteDataRoot, callback);
+	powershell.runAsync(psFile, ' -packagesFileLocation \'' + packagesConfig + '\'' + ' -webRootPath ' + websiteRoot + ' -dataRootPath ' + websiteDataRoot, callback);
 });
 
-gulp.task("default", function () {
-	console.log("You need to specifiy a task.");
+gulp.task('default', function () {
+	console.log('You need to specifiy a task.');
 });
